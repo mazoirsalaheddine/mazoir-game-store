afad8e148c8036a51a1a81c64b2316edf3fc558e
mazoirsalaheddine <salaheddinemazoirr@gmail.com>
2026-06-12 13:08:54 +0100

    fixt

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,65 +1,12 @@
 <?php
-/*
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashbordController;
-use App\Http\Controllers\GameAccountController;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\ResetPasswordController;
 
-
+// هاد الـ Route غايبان فاش تدخل للرابط الرئيسي ديال قاعدة البيانات فـ Railway
 Route::get('/', function () {
-    return view('pages.index');
-})->name('index');
-
-// regestretion
-Route::get('/register', [AuthController::class, 'showRegister'])->name('showRegister');
-Route::post('/registration_post', [AuthController::class, 'registration_post']);
-
-// login
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login_post', [AuthController::class, 'login_post'])->name('login.post');
-
-// logout
-Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
-
-
-// ----------------------------- Forget Password --------------------------//
-// Route::controller(ForgotPasswordController::class)->group(function () {
-//     Route::post('forget-password', 'sendResetLinkEmail')->name('password.email');
-// });
-
-// // ---------------- Forget Password ---------------- //
-// Route::controller(ForgotPasswordController::class)->group(function () {
-
-//     // عرض الفورم
-//     Route::get('forget-password', 'showLinkRequestForm')
-//         ->name('password.request');
-
-//     // إرسال الإيميل
-//     Route::post('forget-password', 'sendResetLinkEmail')
-//         ->name('password.email');
-// });
-// // ---------------------------- Reset Password ----------------------------//
-// Route::controller(ResetPasswordController::class)->group(function () {
-//     Route::get('reset-password/{token}', 'showResetForm')->name('password.reset');
-//     Route::post('reset-password', 'reset')->name('password.update');
-// });
-
-
-
-Route::get('forget-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('forget-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-
-Route::get('reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
-
-// verefication middleware
-Route::group(['middleware' => 'admin'], function(){
-    Route::get('admin/dashboard',[DashbordController::class,'dashboard'])->name('admin.dashboard');
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Mazoir Game Store API is running perfectly!',
+        'environment' => app()->environment()
+    ]);
 });
-
-Route::group(['middleware' => 'user'], function(){
-    Route::get('pages/store',[GameAccountController::class, 'store'])->name('pages.store');
-});
-*/
